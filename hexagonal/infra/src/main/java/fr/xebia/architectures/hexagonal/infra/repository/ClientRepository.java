db192f7f21922016f0f063b17671daf6abf2e565
Benjamin <bdupin@xebia.fr>
2018-09-13 10:21:11 +0200

    Implem hexagonal

--- hexagonal/infra/src/main/java/fr/xebia/architectures/hexagonal/infra/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.xebia.architectures.hexagonal.infra.repository;
-
-
-import fr.xebia.architectures.hexagonal.infra.entity.Client;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClientRepository extends MongoRepository<Client, String> {
-
-}
