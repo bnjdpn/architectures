75b6bd695df7e6a4c1e8f0dc8ec7b9ec49422ef3
Benjamin <bdupin@xebia.fr>
2018-10-31 08:38:14 +0100

    Implem hexagonal architecture

--- hexagonal/domain/src/main/java/fr/xebia/architectures/hexagonal/domain/operation/Withdraw.java
+++ hexagonal/domain/src/main/java/fr/xebia/architectures/hexagonal/domain/operation/Withdraw.java
@@ -4,6 +4,7 @@ import fr.xebia.architectures.hexagonal.domain.account.Account;
 
 import java.util.Currency;
 
+@FunctionalInterface
 public interface Withdraw {
 
     Account make(Account account, String label, double amount, Currency currency);
